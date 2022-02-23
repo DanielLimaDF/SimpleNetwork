7f48b47eaa8603fe2aacfe8fd18a52b34613cc9e
Daniel Lima <eunotomocaf@gmail.com>
2022-02-22 22:56:00 -0300

    add network error descriptions

--- Sources/SimpleNetwork/Protocols/NetworkError.swift
+++ Sources/SimpleNetwork/Protocols/NetworkError.swift
@@ -13,3 +13,20 @@ public enum NetworkError: Error {
     case noContent
     case invalidURL
 }
+
+extension NetworkError: LocalizedError {
+
+    var errorDescription: String? {
+        switch self {
+        case .parseFailed:
+            return "Error on parsing data content"
+        case let .defaultError(error)
+            return error.localizedDescription
+        case .noContent:
+            return "No content"
+        case .invalidURL:
+            return "Invalid URL"
+        }
+    }
+
+}
